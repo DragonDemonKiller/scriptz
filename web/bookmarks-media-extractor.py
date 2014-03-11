bb9f6e55232187517aa796cb17ea2904ce333817
nodiscc <nodiscc@gmail.com>
2014-03-11 15:45:43 +0100

    web/bookmarks-media-extractor.py: work on user selected tags; links tagged 'nodl' will no longer be downloaded (this tag serves as an ignore list)

--- web/bookmarks-media-extractor.py
+++ web/bookmarks-media-extractor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
+import os
 from bs4 import BeautifulSoup
 from subprocess import call
 
-bookmarksfilename = 'bookmarks_public_20140220_210628.html'
+bookmarksfilename = 'bookmarks_all_20140303_003115.html'
+usertag = "music"
 #TODO: pass bookmarks filename from argv
 #TODO: detect if bookmarks filename has correct format
 #TODO: support plain text (not html) lists
@@ -16,9 +18,10 @@ links = data.find_all('a')
 #TODO: change working directory from argv --- os.chdir(path)
 #TODO: get tags from argv
 #TODO: if tag is specified, mkdir workdir/tag, change dir to it
-print '[html extractor] Getting music files...'
+print '[html extractor] Getting %s files...' % usertag
+os.chdir(usertag)
 for item in links:
-    if 'video' in item.get('tags'):
+    if usertag in item.get('tags') and 'nodl' not in item.get('tags'):
         outitem = " * [" + item.contents[0] + "](" + item.get('href') + ")" + " `@" + item.get('tags') + "`"
         print outitem #TODO: print to outfile
         #call(["youtube-dl", "--extract-audio", "--audio-quality", "0", item.get('href')])
