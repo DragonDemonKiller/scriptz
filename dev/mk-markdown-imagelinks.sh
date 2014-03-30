d28198152bb4d724064d84026437913e826bb22d
nodiscc <nodiscc@gmail.com>
2014-03-30 03:27:24 +0200

    update dev/mk-markdown-imagelinks.sh (run it from the same dir as your markdown file)

--- dev/mk-markdown-imagelinks.sh
+++ dev/mk-markdown-imagelinks.sh
@@ -2,13 +2,17 @@
 #Description: Take image files as parameters, generate a thumbnail for each one in thumbs/, and print a markdown link showing the thumbnail and pointing to the image.
 #License: MIT http://opensource.org/licenses/MIT
 #Source: https://github.com/nodiscc/scriptz
+#Usage: run it from the same directory as your markdown file (to make sure the
+#relative markdown link is correct)
+
+set -e
 
 THUMBNAIL_SIZE="200"
 IMAGES_DIR="_media"
 
-if [ ! -d thumbs ]
+if [ ! -d "$IMAGES_DIR/thumbs" ]
 then
-	mkdir thumbs/
+	mkdir "$IMAGES_DIR/thumbs"
 fi
 
 for IMAGE in $@
@@ -19,8 +23,8 @@ do
 	else
 		IMAGE_EXTENSION=`echo "$IMAGE" | awk -F "." '{print $NF}'`
 		IMAGE_BASENAME=`basename "$IMAGE" "$IMAGE_EXTENSION"`
-		convert -thumbnail "$THUMBNAIL_SIZE"x "$IMAGE" "${IMAGE_BASENAME}_thumb.${IMAGE_EXTENSION}"
-		echo "[![](${IMAGES_DIR}/${IMAGE_BASENAME}_thumb.${IMAGE_EXTENSION})](${IMAGES_DIR}/${IMAGE})"
+		convert -thumbnail "$THUMBNAIL_SIZE"x "$IMAGE" "$IMAGES_DIR/thumbs/${IMAGE_BASENAME}_thumb.${IMAGE_EXTENSION}"
+		echo "[![](${IMAGES_DIR}/thumbs/${IMAGE_BASENAME}_thumb.${IMAGE_EXTENSION})](${IMAGE})"
 	fi
 done
 
