06787debc8b6e761f1cf772a72017f04b9b374c9
nodiscc <nodiscc@gmail.com>
2014-04-08 12:16:21 +0200

    wpa-sup.sh: reorder user input requests, fix wpa_passphrase command line (args were upside down), fix wpa_supplicant command line (-w is invalid)

--- sysadmin/wpa-sup.sh
+++ sysadmin/wpa-sup.sh
@@ -5,13 +5,17 @@
 #TODO: let user choose wifi interface from a list
 #TODO: let user choose Network from a list
 
-echo "Enter WPA Passphrase"
-read mypassphrase
 
-echo "Enter network name (ESSID)"
+echo -n "Enter network name (ESSID): "
 read myssid
 
-echo "Enter wifi card name (leave blank for default: wlan0)"
+
+echo -n "Enter WPA Passphrase: "
+read mypassphrase
+
+
+
+echo -n "Enter wifi card name (leave blank for default: wlan0): "
 read interface
 
 if [ "$interface" = "" ]
@@ -20,10 +24,10 @@ fi
 
 
 echo "Calculating PSK...."
-mypsk=`wpa_passphrase $mypassphrase $myssid | sed -n 4p | sed -e 's/\tpsk=//g'`
+mypsk=`wpa_passphrase "$myssid" "$mypassphrase" | sed -n 4p | sed -e 's/\tpsk=//g'`
 
-echo "creating /etc/wpa_supplicant.conf"
-sudo echo 'ctrl_interface=/var/run/wpa_supplicant
+echo "Creating /etc/wpa_supplicant.conf..."
+sudo echo "ctrl_interface=/var/run/wpa_supplicant
 #ap_scan=2
 
 network={
@@ -35,7 +39,7 @@ network={
        group=CCMP TKIP
        psk=$mypsk
 }
-' > /etc/wpa_supplicant.conf
+" > /etc/wpa_supplicant.conf
 
 echo "Trying to connect..."
-sudo wpa_supplicant -Bw -Dwext -i $interface -c/etc/wpa_supplicant.conf
+sudo wpa_supplicant -B -Dwext -i "$interface" -c/etc/wpa_supplicant.conf
