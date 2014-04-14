95dc19b8239c7d9bbcc01bf09261d55085add032
nodiscc <nodiscc@gmail.com>
2014-04-14 19:48:19 +0200

    web/webpage-to-markdown.sh: add TODOs

--- web/webpage-to-markdown.sh
+++ web/webpage-to-markdown.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #fetches a webpage and converts it to markdown
+#TODO: add the original URL to the header, see man pandoc, TEMPLATES section
+#TODO: name the file after the page's title
 URL="$1"
 OUTFILE="$2"
 USAGE="USAGE: `basename $0` page_url output_file"
