c625efd5115d66cc9e76b454ccb8653f15424095
nodiscc <nodiscc@gmail.com>
2014-05-01 20:05:01 +0200

    /dev/markdown-describe-scripts.sh: only show files, not directories

--- dev/markdown-describe-scripts.sh
+++ dev/markdown-describe-scripts.sh
@@ -5,8 +5,11 @@
 
 #Lines with descriptions have to start with "#Description: "
 
-for i in *
+for i in $(find . -maxdepth 1 -type f -exec basename '{}' \;)
 do
 	md_desc=$(grep "^#Description" "$i" 2>/dev/null | cut -d " " -f 1 --complement)
 	echo " * [$i]($i) - $md_desc"
 done
+
+#TODO: sort output
+#Also works for .deb packages: for i in *; do pkgdesc=$(dpkg -I "$i" | grep "^ Description" | cut -d ":" -f 1 --complement); echo " * [$i]($i) - $pkgdesc"; done
